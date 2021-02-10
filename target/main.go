7577388c89b61eb61bf969d8e30ed88309f954b5
Matthias Rupp <matthias.rupp@posteo.de>
2021-02-10 15:40:12 +0100

    Slow dos prevention

--- target/main.go
+++ target/main.go
@@ -9,6 +9,9 @@ import (
 	"strconv"
 	"mime"
 	"path/filepath"
+	"time"
+	"bytes"
+	"sync"
 )
 
 type headerField struct {
@@ -16,47 +19,114 @@ type headerField struct {
 	value string
 }
 
+type connectionData struct {
+	conn *net.Conn
+	addr string
+	tst int64
+}
+
 var (
-	numConnections = 0
+	connections = make([]connectionData, 0)
+	connectionManagementMutex sync.Mutex
 )
 
 const (
-	maxNumberConnections = 2
+	maxNumberConnections = 30
+	paramU = 20
+	paramL = 10
+	paramT = 5
 )
 
 func main() {
 	sock, _ := net.Listen("tcp", "0.0.0.0:7000")
 	defer sock.Close()
 
-	for {
+	go dosProtector()
 
+	for {
 		// Wait for new free connections
-		for numConnections >= maxNumberConnections {}
-		
+		for len(connections) >= maxNumberConnections {}
+
+		// Accept new connection
 		connection, err := sock.Accept()
 		
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			numConnections++
-			fmt.Println("Connections: " + strconv.Itoa(numConnections))
-			go requestWorker(connection)
+			connectionData := registerConnection(connection)
+			
+			// Handle request
+			go requestWorker(connectionData)
 		}
 	}
 }
 
-func requestWorker(connection net.Conn) {
-	reader := bufio.NewReader(connection)
+func dosProtector() {
+	dosState := false
+	
+	for {
+		if !dosState && len(connections) > paramU {
+			dosState = true
+			fmt.Println("Slow DOS detected!")
+		}
+		if dosState && len(connections) < paramL {
+			dosState = false
+			fmt.Println("Slow DOS ended.")
+		}
+		if dosState {
+			connectionsByIP := make(map[string]int)
+			
+			// Find number of connections by source IP
+			for i := 0; i < len(connections); i++ {
+				connection := connections[i]
+				
+				val, ok := connectionsByIP[connection.addr]
+				if ok {
+					connectionsByIP[connection.addr] = val + 1
+				} else {
+					connectionsByIP[connection.addr] = 1
+				}
+			}
+
+			// Find IP with most connections
+			maxNum := 0
+			maxIP := ""
+			
+			for ip, num := range connectionsByIP {
+				if num > maxNum {
+					maxNum = num
+					maxIP = ip
+				}
+			}
+
+			// Kill all connections with this IP where duration > paramT
+			for i := 0; i < len(connections); i++ {
+				connection := connections[i]
+
+				if connection.addr == maxIP {
+					connectionDuration := time.Now().Unix() - connection.tst
+
+					if connectionDuration > paramT {
+						closeConnection(connection)
+					}
+				}
+			}
+		}
+	}
+}
+
+func requestWorker(connection connectionData) {
+	defer closeConnection(connection)
+	
+	reader := bufio.NewReader(*connection.conn)
 
 	// Read first line GET / HTTP/1.1
 	method, _ := reader.ReadString(' ')
 	resource, _ := reader.ReadString(' ')
-	http, _ := reader.ReadString('\r')
+	reader.ReadString('\r')
 
 	// Read newline before headers
 	reader.ReadLine()
-
-	fmt.Println(method + " " + resource + " " + http)
 
 	// Read additional data (headers, post body...)
 
@@ -85,17 +155,18 @@ func requestWorker(connection net.Conn) {
 		resource = "index.html"
 	}
 
+	if resource == "/connections" && method == "GET" {
+		sendConnectionList(*connection.conn)
+	}
+
 	if method == "GET" {
-		sendFile(connection, resource)
+		sendFile(*connection.conn, resource)
 	}
 
 	if method == "POST" && resource == "/echo" {
 		data := receivePostData(reader, contentLength)
-		sendEchoReply(connection, contentType, data)
+		sendEchoReply(*connection.conn, contentType, data)
 	}
-	
-	connection.Close()
-	numConnections--
 }
 
 func sendFile(c net.Conn, filename string) {
@@ -123,13 +194,10 @@ func receivePostData(reader *bufio.Reader, length int) []byte {
 	for i := 0; i < length;  i++ {
 		b, err := reader.ReadByte()
 		
-		if err != nil {
-			fmt.Println(err)
+		if err == nil {
+			buff[i] = b
 		}
-		buff[i] = b
 	}
-
-	fmt.Println("POST read done.")
 
 	return buff
 }
@@ -142,6 +210,43 @@ func sendEchoReply(c net.Conn, contentType string, data []byte) {
 	c.Write(data)
 }
 
+func sendConnectionList(c net.Conn) {
+	var buffer bytes.Buffer
+
+	buffer.WriteString("<style>td, th { padding: 5px}</style>")
+	buffer.WriteString("<h1>Current connections<h1>")
+	buffer.WriteString("<table border=\"1\">")
+	buffer.WriteString("<tr><th>Connection index</th><th>Source IP</th><th>Duration</th></tr>")
+
+	for i := 0; i < len(connections); i++ {
+		buffer.WriteString("<tr>")
+		
+		buffer.WriteString("<td>")
+		buffer.WriteString(strconv.Itoa(i))
+		buffer.WriteString("</td>")
+
+		buffer.WriteString("<td>")
+		buffer.WriteString(connections[i].addr)
+		buffer.WriteString("</td>")
+
+		buffer.WriteString("<td>")
+		buffer.WriteString(strconv.Itoa(int(time.Now().Unix() - connections[i].tst)))
+		buffer.WriteString("</td>")
+
+		buffer.WriteString("</tr>")
+	}
+
+	buffer.WriteString("</table>")
+	
+	data := buffer.Bytes()
+	
+	c.Write([]byte("HTTP/1.1 200 OK\r\n"))
+	c.Write([]byte("Content-Type: text/html\r\n"))
+	c.Write([]byte("Content-Length: " + strconv.Itoa(len(data)) + "\r\n"))
+	c.Write([]byte("\r\n"))
+	c.Write(data)
+}
+
 func parseHeaderLine(header string) *headerField {
 	split := strings.Split(header, ": ")
 	
@@ -149,4 +254,37 @@ func parseHeaderLine(header string) *headerField {
 		key: strings.ToLower(split[0]),
 		value: split[1],
 	}
+}
+
+func closeConnection(connection connectionData) {
+	connectionManagementMutex.Lock()
+	(*connection.conn).Close()
+
+	for i := 0; i < len(connections); i++ {
+		if connection == connections[i] {
+			connections = append(connections[:i], connections[i+1:]...)
+			break
+		}
+	}
+
+	fmt.Println("Connections: " + strconv.Itoa(len(connections)))
+	connectionManagementMutex.Unlock()
+} 
+
+func registerConnection(connection net.Conn) connectionData {
+	connectionManagementMutex.Lock()
+
+	cData := connectionData{
+		conn : &connection,
+		addr : strings.Split(connection.RemoteAddr().String(), ":")[0],
+		tst  : time.Now().Unix(),
+	}
+	
+	// Add connection to list
+	connections = append(connections, cData)
+	
+	fmt.Println("Connections: " + strconv.Itoa(len(connections)))
+	connectionManagementMutex.Unlock()
+	
+	return cData
 }
